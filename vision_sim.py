11747d0a3cfd9d44c9a19357ae80dc6eda42bd17
Mendru <mendruxie@yahoo.com>
2018-05-17 18:53:24 +0800

    Update vision_sim.py

--- vision_sim.py
+++ vision_sim.py
@@ -4,10 +4,12 @@ from keras.applications.resnet50 import preprocess_input
 from keras.models import Model, load_model
 from keras.preprocessing import image
 import argparse
+import requests
+from io import BytesIO
 
 def extract_feature(img_path, boxes, net):
 #    img = image.load_img(img_path, target_size=(224, 224))
-    img = image.load_img(img_path)
+    img = image.load_img(BytesIO(requests.get(img_path).content))
     feature = []
     for box in boxes:
         box[2] = box[0] + box[2]
@@ -22,7 +24,7 @@ def extract_feature(img_path, boxes, net):
     return feature
 
 def get_similarity(img_path1, img_path2, boxes_1, boxes_2):
-    net = load_model('market_pair_pretrain.h5')
+    net = load_model('D:\Mendru\python code\simple_simlaries\market_pair_pretrain.h5')
     net = Model(inputs=[net.input], outputs=[net.get_layer('avg_pool').output])
     feature_1 = extract_feature(img_path1, boxes_1, net)
     feature_2 = extract_feature(img_path2, boxes_2, net)
@@ -34,12 +36,16 @@ def get_similarity(img_path1, img_path2, boxes_1, boxes_2):
             sim[i][j] = np.sum(f1*f2)
     return sim
 
+def partition(box):
+    return [box[i:i+4] for i in range(0,len(box),4)]
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='compute similarity of two images, example:\n python vision_sim.py --img1 /home/cwh/0.jpg --box1 [[382,193,73,126]] --img2 /home/cwh/2.jpg --box2 [[370,196,92,125]]')
+    parser = argparse.ArgumentParser(description='compute similarity of two images, example:\n python vision_sim.py --img1 /home/cwh/0.jpg --box1 382 193 73 126 --img2 /home/cwh/2.jpg --box2 370 196 92 125')
     parser.add_argument('--img1', help='path of image1')
     parser.add_argument('--img2', help='path of image2')
-    parser.add_argument('--box1', type=list, help='boxes of image1')
-    parser.add_argument('--box2', type=list, help='boxes of image2')
+    parser.add_argument("--box1", nargs='*', type=int)  
+    parser.add_argument("--box2", nargs='*', type=int)
     args = parser.parse_args()
-    print(get_similarity(args.img1, args.img2, args.box1, args.box2))
-
+    boxes_1=partition(args.box1)
+    boxes_2=partition(args.box2)
+    print(get_similarity(args.img1, args.img2, boxes_1, boxes_2))
